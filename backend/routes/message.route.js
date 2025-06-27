ec00140e580b16897af4ab5ac48cd6c26799f2fb
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-06-27 15:32:03 +0530

    small update

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { protect } from '../middlewares/auth.middleware';
+import { protect } from '../middlewares/auth.middleware.js';
 import { sendMessage, getMessages } from '../controllers/message.controller.js';
 
 const router = express.Router();
