2ebdf2c30526e770aa9cf92bedbe0c458dbf877b
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-07-08 23:01:11 +0530

    minor update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'https://sologrind.onrender.com',
+      '/': 'https://sologrind.onrender.com',
     },
   },
 })
